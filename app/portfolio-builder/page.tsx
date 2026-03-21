cab72c533f5b4b2ad27c8a627911c8e91f56c2ed
Snehal Chetry <snehalrajuchetry@gmail.com>
2026-03-21 22:56:55 +0530

    chore: export portfolio builder to standalone project and cleanup

--- app/portfolio-builder/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { PortfolioBuilder } from '@/components/portfolio-builder/portfolio-builder';
-
-export default function PortfolioBuilderPage() {
-  return <PortfolioBuilder />;
-}
